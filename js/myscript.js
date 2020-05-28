63a135ff4f851946c03f6de538d902b101df232c
yue-lim <64457705+yue-lim@users.noreply.github.com>
2020-05-28 18:32:24 +0900

    0528(1)

--- js/myscript.js
+++ js/myscript.js
@@ -2,15 +2,20 @@
 // 껍데기 body영역 맨끝에 넣었으니까 굳이 없어도 됨
 // })
 
+//header menu 이벤트
 $('#header .menu >li >a').on("mouseover focus", function(){
    $(this).parent().addClass("on")
    $(this).parent().siblings().removeClass("on")
-   $(this).next().stop().show(400)
+   $(this).next().stop().show()
 })
 
-$('#header .menu >li >a').on("mouseleave" , function(){
-   $(this).next().stop().hide(400)
-   $(this).parent().removeClass("on")
+$('#header .menu >li').on("mouseleave" , function(){
+   $(this).find('.submenu').stop().hide()
+   $(this).removeClass("on")
+})
+
+$(".openMenu").on("click", function(){
+   $(this).siblings(".menu").show()
 })
 
 
