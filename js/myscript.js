5e7e79a88391618ece52f293e9536c6bbb60ee68
yue-lim <64457705+yue-lim@users.noreply.github.com>
2020-06-01 16:39:18 +0900

    0601(rwd;menu;submenu)

--- js/myscript.js
+++ js/myscript.js
@@ -4,20 +4,48 @@
 
 //header menu 이벤트
 $('#header .menu >li >a').on("mouseover focus", function(){
+   if (winWidth > 1210) {
    $(this).parent().addClass("on")
    $(this).parent().siblings().removeClass("on")
    $(this).next().stop().show()
+   }
 })
+
+//1210이하 menu이벤트 
+$('.menu >li >span').on("click", function(){
+   $(this).siblings().removeClass("on")
+   $(this).toggleClass("on")
+   $(this).parent().find('.submenu').stop().slideToggle()
+   $(this).parent().siblings().find('.submenu').slideUp()
+   
+})
+
+var winWidth 
+function wid() {
+   winWidth = $(window).width()
+}
 
 $('#header .menu >li').on("mouseleave" , function(){
-   $(this).find('.submenu').stop().hide()
+   if (winWidth > 1210) {
+      $(this).find('.submenu').stop().hide()
    $(this).removeClass("on")
+   }
 })
 
+//햄버거버튼 시작
 $(".openMenu").on("click", function(){
    $(this).parents(".header_nav").find(".closeBtn").show()
    $(this).siblings(".menu").show().animate({width: "45%"}, 500, "easeInOutCubic")
 })
+
+//클로즈버튼 시작
+$('.closeBtn').on("click", function(){
+   $(this).hide()
+   $(this).parent().find('.menu').hide().animate({width:"0%"}, 500, "easeInOutCubic")
+})
+
+
+
 
 
 
